b357dd4f36c57c969a4b0154402c4af4215143d2
andylorrayne <26768232+andylorrayne@users.noreply.github.com>
2019-05-14 15:04:20 -0300

    Add files via upload
    sc

---
+++ WebBiblio/js/login.js
@@ -0,0 +1,26 @@
+var count= 2;
+
+function validate(){
+	var us = document.index.usuario.value;
+	var sh = document.index.senha.value;
+	var valid = false;
+	var usuarioArray = ["andy", "lorrayne"];
+	var senhaArray = ["1234", "5678"];
+
+	for (var i = 0; i < usuarioArray.lenght; i++) {
+		if((us == usuarioArray[i]) && (sh == senhaArray[i])){
+
+			valid = true;
+			break;
+		}
+	}
+
+	if(valid){
+		alert("Login feito com sucesso!");
+		window.location ="./sistem/principal.html";
+		return false;
+	}
+
+
+
+}

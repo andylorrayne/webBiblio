c827a377fbfa70b6596ce40917410adc3e1b4535
andylorrayne <26768232+andylorrayne@users.noreply.github.com>
2019-05-16 11:35:26 -0300

    Update login.js

--- WebBiblio/js/login.js
+++ WebBiblio/js/login.js
@@ -1,24 +1,24 @@
 var count= 2;
 
 function validate(){
-	var us = document.index.usuario.value;
-	var sh = document.index.senha.value;
-	var valid = false;
-	var usuarioArray = ["andy", "lorrayne"];
-	var senhaArray = ["1234", "5678"];
+	var us = document.index.usuario.value
+	var sh = document.index.senha.value
+	var valid = false
+	var usuarioArray = ["andy", "lorrayne"]
+	var senhaArray = ["1234", "5678"]
 
 	for (var i = 0; i < usuarioArray.lenght; i++) {
 		if((us == usuarioArray[i]) && (sh == senhaArray[i])){
 
-			valid = true;
-			break;
+			valid = true
+			break
 		}
 	}
 
 	if(valid){
-		alert("Login feito com sucesso!");
-		window.location ="./sistem/principal.html";
-		return false;
+		alert("Login feito com sucesso!")
+		window.location ="./sistem/principal.html"
+		return false
 	}
 
 

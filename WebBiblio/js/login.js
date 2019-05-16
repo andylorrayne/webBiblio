b6028c99ef1f81767efe6d222fae04688253f280
andylorrayne <26768232+andylorrayne@users.noreply.github.com>
2019-05-16 13:48:17 -0300

    Update login.js
    função loginform

--- WebBiblio/js/login.js
+++ WebBiblio/js/login.js
@@ -1,26 +1,53 @@
 var count= 2;
+console.log("CARREGOU SCRIPT")
 
-function validate(){
-	var us = document.index.usuario.value
-	var sh = document.index.senha.value
-	var valid = false
-	var usuarioArray = ["andy", "lorrayne"]
-	var senhaArray = ["1234", "5678"]
-
-	for (var i = 0; i < usuarioArray.lenght; i++) {
-		if((us == usuarioArray[i]) && (sh == senhaArray[i])){
-
-			valid = true
-			break
-		}
-	}
-
-	if(valid){
-		alert("Login feito com sucesso!")
-		window.location ="./sistem/principal.html"
-		return false
+var ObjPessoas = [
+	
+	{
+		usuario: "andy",
+		senha : "123"
+	},
+	{
+		usuario: "sam",
+		senha : "456"
+	},
+	{
+		usuario: "may",
+		senha : "789"
 	}
 
 
+]
 
+function loginform(){
+	//deixar tudo em letra minuscula
+	/*loginform1.usuario.value = loginform1.usuario.value.toLowerCase()
+	loginform1.senha.value = loginform1.senha.value.toLowerCase()
+
+     if (loginform1.usuario.value == "marta" && loginform1.senha.value == "123") {
+
+     	location = "principal.html"
+     }else{
+     	loginform1.usuario.value=""
+     	loginform1.senha.value=""
+     	alert("Dados incorretos");
+
+     }*/
+
+
+     var usuario = document.getElementById("usuario").value
+     var senha = document.getElementById("senha").value
+
+     //alert("usuario é: " + usuario + "senha : " + senha);
+
+     for (var i = 0; i < ObjPessoas.length; i++ ){
+     	if (usuario == ObjPessoas[i].usuario && senha == ObjPessoas[i].senha) {
+     		alert(usuario + " "+ "está logado!");
+     		location="principal.html"
+     		return 
+     	}
+     }
+     usuario.value = ""
+   	 senha.value=""
+     alert("DADOS INCORRETOS");
 }
